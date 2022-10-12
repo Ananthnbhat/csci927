80540f61f5c38ed41ac18b4a1663046da744f700
Ananthnbhat <ananthnbhat2@gmail.com>
2022-10-13 10:17:25 +1100

    add controller for home page

---
+++ pandemica-service1/src/main/java/com/csci927/pandemicaservice1/HomeController.java
@@ -0,0 +1,14 @@
+package com.csci927.pandemicaservice1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "home";
+    }
+
+}
